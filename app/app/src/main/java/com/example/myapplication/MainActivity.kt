2d41041e7a542e7bded0008558e9d53cc28aa652
GuillaumeCerdan <gcerdan83@gmail.com>
2019-07-13 02:16:31 +0200

    First Push --> view

---
+++ app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        /*mon_icone.setOnClickListener() {
+            // Lance l'activité de connection mais pour l'instant c'est dans main_activity
+        }*/
+    }
+}
